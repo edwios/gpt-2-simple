a483fa53b1331d45e29241498992bcd320cfa1e8
Edwin Tam <edwintam@iostation.com>
2019-06-05 14:30:40 +0800

    Added utf8 to sample generation

--- gpt_2_simple/gpt_2.py
+++ gpt_2_simple/gpt_2.py
@@ -232,7 +232,7 @@ def finetune(sess,
         maketree(os.path.join(SAMPLE_DIR, run_name))
         with open(
                 os.path.join(SAMPLE_DIR, run_name,
-                             'samples-{}').format(counter), 'w') as fp:
+                             'samples-{}').format(counter), encoding='utf-8', 'w') as fp:
             fp.write('\n'.join(all_text))
 
     def sample_batch():
